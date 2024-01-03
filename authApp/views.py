4ceb0f24034a23d6f20c05c46202508f59a136a5
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2024-01-03 17:10:27 +0530

    SETUP : RestFramework

--- authApp/views.py
+++ authApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+class UserView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        return Response(users.values())
